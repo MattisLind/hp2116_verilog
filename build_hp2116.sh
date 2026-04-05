0de4cc66a4429023ee75a967f844ef7bccd9733d
Mattis Lind <mattis@iotcomms.io>
2026-04-05 19:04:34 +0200

    Adding build shell script.

---
+++ build_hp2116.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker run --rm -it \
+  --entrypoint bash \
+  -v "$PWD":/work -w /work \
+  verilator/verilator:latest \
+  -lc '
+    rm -rf obj_dir &&
+    verilator -Wall -Wno-fatal -Wno-UNUSEDSIGNAL --no-sched-zero-delay \
+      --sv --timing --binary --trace-fst \
+      tb_hp2116.sv hp2116_cpu.sv hp12531c.sv \
+      --top-module tb_hp2116 && \
+    ./obj_dir/Vtb_hp2116
+  '
