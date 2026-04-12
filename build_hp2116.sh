73e730b1cd6a4d78285e994d5ac40df202a77187
Mattis Lind <mattis@iotcomms.io>
2026-04-12 11:25:58 +0200

    Removed threading since it made it slower.

--- build_hp2116.sh
+++ build_hp2116.sh
@@ -7,7 +7,7 @@ docker run --rm -it \
   -lc '
     rm -rf obj_dir &&
     verilator -Wall -Wno-fatal -Wno-UNUSEDSIGNAL --no-sched-zero-delay \
-      --sv --timing --binary --trace-fst --threads 4 -j 4 \
+      --sv --timing --binary --trace-fst\
       tb_hp2116.sv hp2116_cpu.sv hp12531c.sv hp12597a.sv \
       --top-module tb_hp2116 && \
     ./obj_dir/Vtb_hp2116 "+PTR_FILE=$1" "+DSN=$2" "+PRETEST=$3" "+TRACE=$4"
