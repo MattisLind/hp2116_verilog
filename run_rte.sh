fa2c692a00d02e13ff89234bf1d0d9ffc8cdee3c
Mattis Lind <mattis@iotcomms.io>
2026-05-29 10:50:11 +0200

    Removing debug output.

--- run_rte.sh
+++ run_rte.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build_hp2116.sh diagnostics/24296-60001_DSN000200_DIAGNOSTIC_CONFIGURATOR.abin 000200 NO YES  mh_boot.abs.new RTE_MH.disc
+./build_hp2116.sh diagnostics/24296-60001_DSN000200_DIAGNOSTIC_CONFIGURATOR.abin 000200 NO NO  mh_boot.abs.new RTE_MH.disc
